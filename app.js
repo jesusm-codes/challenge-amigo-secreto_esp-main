d70fe3ee74cb212411e79bb65d52973906535840
jesusm-codes <jesusmslms@gmail.com>
2025-09-21 18:47:46 -0600

    Actualizar app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let listaDeAmigos = [];
-let nombres = 0;
 
 
 function enlistar(nombres) {
